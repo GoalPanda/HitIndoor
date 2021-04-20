bbac27e7fa228560a1bd2c1710cda1f538b96be8
GoalPanda <goalpanda111@gmail.com>
2021-04-19 19:00:16 -0700

    complete https

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,4 +1,5 @@
-const SERVER_BASE_URL = 'http://localhost:5000/api/v1/'
+//const SERVER_BASE_URL = 'http://localhost:5000/api/v1/'
+const SERVER_BASE_URL = 'https://mbpos.hitindoor.com/api/v1/'
 
 export {
   SERVER_BASE_URL,
